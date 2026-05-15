46c25567d006e6d7960e39761345ddf20eb8c5a6
diogoguilherme-dev <diogodg0707@gmail.com>
2026-05-15 16:03:36 -0300

    iniciando o desenvolvimento do movies-project.

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import perfectionist from 'eslint-plugin-perfectionist'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist', 'node_modules', 'vite.config.js']
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      perfectionist
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      // Em vez de importar todas as regras do React de uma vez,
+      // vamos usar apenas o necessário para evitar o bug:
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'warn',
+      'no-unused-vars': 'warn',
+
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'alphabetical',
+          order: 'asc',
+          ignoreCase: true,
+          newlinesBetween: 1,
+          groups: [
+            'type',
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index']
+            // REMOVI 'object' e 'unknown' daqui, pois eles causavam o erro
+          ]
+        }
+      ]
+    },
+    settings: {
+      react: { version: 'detect' }
+    }
+  },
+  eslintConfigPrettier
+]
